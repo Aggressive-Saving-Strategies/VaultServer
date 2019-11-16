469ebb2ca2614708deb33dcad869a7f1381cab7f
joonardo <joonatan.bergholm@taitounited.fi>
2019-11-16 16:28:25 +0200

    api

--- vaultserver/__main__.py
+++ vaultserver/__main__.py
@@ -1,1 +1,17 @@
-print('Hello werld!')
+import bottle
+from vaultserver import api
+from vaultserver import consts
+
+@bottle.get('/account/credits')
+def credits():
+    return api.get_credits(consts.primary_account)
+
+@bottle.post('/account/lose')
+def lose():
+    lost = bottle.request.json['amount']
+    return api.move(consts.primary_account, consts.savings_account, lost)
+
+@bottle.post('/account/win')
+def lose():
+    won = bottle.request.json['amount']
+    return api.move(consts.savings_account, consts.primary_account, won)
